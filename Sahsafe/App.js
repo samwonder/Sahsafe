ddac671d728fe6f844e2bf8484a111ab334f8d92
Praveen <praveen.deshmukh88@gmail.com>
2021-12-07 22:22:25 +0530

    Initial commit

---
+++ Sahsafe/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+ import React, { Component } from 'react';
+ import { Provider } from 'react-redux';
+ import { store } from './app/store';
+ import runRootSaga from './app/sagas';
+ import Navigator from './app/navigator';
+ 
+ export default class App extends Component {
+   constructor() {
+     super();
+     this.state = {};
+     runRootSaga();
+     // for hide warning messages
+     console.disableYellowBox = true;
+   }
+ 
+   render() {
+     return (
+       <Provider store={store}>
+         <Navigator />
+       </Provider>
+     );
+   }
+ }
---
+++ Sahsafe/app/navigator/index.js
@@ -0,0 +1,39 @@
+
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Splash from '../screens/Splash';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
